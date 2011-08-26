b1bc006744c0ada68392d5267ac4eb757a0ada7e
devin <dev-null@apache.org>
2011-08-26 06:05:38 +0000

    changeset:   2:f31f7bb96749 user:        Tony date:        Fri Jun 05 11:09:46 2009 +0800 change to online link
    git-svn-id: https://svn.apache.org/repos/asf/incubator/odf/trunk@1167925 13f79535-47bb-0310-9956-ffa450edef68

--- taglets/src/main/java/org/odftoolkit/doctag/AttributeNameTaglet.java
+++ taglets/src/main/java/org/odftoolkit/doctag/AttributeNameTaglet.java
@@ -96,7 +96,7 @@ public class AttributeNameTaglet implements Taglet {
 	        //return "<u>" + tag.text() + "</u>";
 		int pos = tag.text().lastIndexOf(":");
 		String link = "attribute-"+tag.text().substring(0, pos)+"_"+tag.text().substring(pos+1);
-	    	return "<a href=\"../../../../../../OpenDocument-v1.2-cd02.xhtml#"+ link +"\">"+ tag.text()+ "</a>";
+	    	return "<a href=\"http://odftoolkit.org/downloads/odfdom/OpenDocument-v1.2-draft/OpenDocument-v1.2-cd02.xhtml#"+ link +"\">"+ tag.text()+ "</a>";
 	    }
 	    
 	    /**
