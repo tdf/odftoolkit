fc256d82cc34a0893df91b49288d5bf45a4dab69
devin <dev-null@apache.org>
2011-08-26 13:24:18 +0000

    changeset:   87:584b6a8fc063 user:        tony date:        Mon Apr 06 23:25:55 2009 +0800 add create children element methods
    git-svn-id: https://svn.apache.org/repos/asf/incubator/odf/trunk@1167774 13f79535-47bb-0310-9956-ffa450edef68

--- old-generator/core/src/org/odftoolkit/odfdom/dom/element/math/OdfMathElement.java
+++ old-generator/core/src/org/odftoolkit/odfdom/dom/element/math/OdfMathElement.java
@@ -32,25 +32,21 @@ import org.odftoolkit.odfdom.dom.OdfNamespace;
 import org.odftoolkit.odfdom.doc.OdfFileDom;
 import org.odftoolkit.odfdom.dom.element.OdfElement;
 
+;
 
 /**
  * ODF DOM Element implementation for element "<math:math>".
  */
 public abstract class OdfMathElement extends OdfElement
 {        
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = -5940264130972149109L;
-	public static final OdfName ELEMENT_NAME = OdfName.get( OdfNamespace.MATH, "math" );
+    public static final OdfName ELEMENT_NAME = OdfName.get( OdfNamespace.MATH, "math" );
 
     public OdfMathElement( OdfFileDom _aOwnerDoc )
     {
         super( _aOwnerDoc, ELEMENT_NAME );
     }
 
-    @Override
-	public OdfName getOdfName()
+    public OdfName getOdfName()
     {
         return ELEMENT_NAME;
     }
