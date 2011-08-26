9af4de435b3656ccc0d61a67d78d43a86cb30d9d
devin <dev-null@apache.org>
2011-08-26 06:07:07 +0000

    changeset:   18:f842f537dc8a user:        svanteschubert date:        Fri Feb 19 22:36:24 2010 +0100 Version 0.8 and Copyright change Sun to Oracle
    git-svn-id: https://svn.apache.org/repos/asf/incubator/odf/trunk@1167939 13f79535-47bb-0310-9956-ffa450edef68

--- taglets/src/main/java/org/odftoolkit/odfdom/taglet/OdfElementTaglet.java
+++ taglets/src/main/java/org/odftoolkit/odfdom/taglet/OdfElementTaglet.java
@@ -2,6 +2,7 @@
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * Copyright 2009 IBM. All rights reserved.
+ * Copyright 2010 Oracle and/or its affiliates. All rights reserved.
  * 
  * Use is subject to license terms.
  * 
