20aa52ed0a6409efacf26a0dcc3a6a365317cd0d
devin <dev-null@apache.org>
2011-08-26 12:05:58 +0000

    changeset:   8:32da6cbba5e1 user:        Lars Behrmann <Lars.Behrmann@Sun.COM> date:        Sat Nov 01 22:27:30 2008 +0100 - check in
    git-svn-id: https://svn.apache.org/repos/asf/incubator/odf/trunk@1167698 13f79535-47bb-0310-9956-ffa450edef68

---
+++ old-generator/src/org/openoffice/odf/doc/dom/type/OdfBoolean.java
@@ -0,0 +1,45 @@
+/************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * Use is subject to license terms.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0. You can also
+ * obtain a copy of the License at http://odftoolkit.org/docs/license.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ************************************************************************/
+package org.openoffice.odf.dom.type;
+        
+public class OdfBoolean {
+    
+    public static String toString( boolean _aBool )
+    {
+        return Boolean.toString( _aBool );
+    }
+
+    public static Boolean valueOf( String _aStringValue )
+    {
+        if( _aStringValue.length()==0 )
+            return null;
+        
+        String aTmp = _aStringValue.trim();
+        
+        if( aTmp.equals("true") || aTmp.equals("1") )
+            return true;
+        else if( aTmp.equals("false") || aTmp.equals("0") )
+            return false;
+        else        
+            throw new IllegalArgumentException( "Illegal ODFBoolean value");
+    }
+}
---
+++ old-generator/src/org/openoffice/odf/doc/dom/type/OdfLengthOrPercent.java
@@ -0,0 +1,104 @@
+/************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * Use is subject to license terms.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0. You can also
+ * obtain a copy of the License at http://odftoolkit.org/docs/license.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ************************************************************************/
+
+package org.openoffice.odf.dom.type;
+
+import org.openoffice.odf.dom.type.OdfMeasure;
+import org.openoffice.odf.dom.util.OdfUnitConverter;
+
+/** This value type stores either a measure value in 1/100th mm as int or a
+ *  percent value as double.
+ */
+public class OdfLengthOrPercent
+{
+    private Object Value;
+    
+    public OdfLengthOrPercent()
+    {        
+        Value = null;
+    }
+   
+    public void setLength( int length )
+    {
+        Value = new Integer(length);
+    }
+    
+    public void setPercent( double percent )
+    {
+        Value = new Double(percent);
+    }
+    
+    public boolean isPercent()
+    {
+        return (Value != null) && Value instanceof Double;
+    }
+    
+    public int getLength()
+    {
+        if( isPercent() )
+        {
+            throw new IllegalArgumentException("I do not have a length value");
+        }
+        else if( Value != null )
+        {
+            return ((Integer)Value).intValue();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    public double getPercent()
+    {
+        if( isPercent() )
+        {
+            return ((Double)Value).doubleValue();
+        }
+        else
+        {
+            throw new IllegalArgumentException("I do not have a percent value");
+        }
+    }
+    
+    public static String toString( OdfLengthOrPercent val )
+    {
+        if( val == null )
+            throw new IllegalArgumentException( "Illegal length or percent value");
+        
+        if( val.isPercent() )
+            return String.valueOf( val.getPercent() ) + "%";
+        else
+            return OdfUnitConverter.getMeasureString(val.getLength());
+    }
+
+    public static OdfLengthOrPercent valueOf( String value )
+    {
+        OdfLengthOrPercent ret = new OdfLengthOrPercent();
+        int n = value.indexOf( "%" );
+	if( n != -1 )
+            ret.setPercent( OdfPercent.valueOf(value));
+        else
+            ret.setLength(OdfMeasure.valueOf(value));
+        return ret;
+    }
+}
