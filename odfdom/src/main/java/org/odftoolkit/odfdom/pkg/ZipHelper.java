69320af9dfb033dc99c9f5be449b26e30066b83d
Svante Schubert <svanteschubert@apache.org>
2015-08-02 11:20:09 +0000

    ODFTOOLKIT-401 - NullPointerException from typo hides all ZIP exceptions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/odf/trunk@1693805 13f79535-47bb-0310-9956-ffa450edef68

--- odfdom/src/main/java/org/odftoolkit/odfdom/pkg/ZipHelper.java
+++ odfdom/src/main/java/org/odftoolkit/odfdom/pkg/ZipHelper.java
@@ -1,20 +1,20 @@
 /************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
- * 
+ *
  * Copyright 2008, 2010 Oracle and/or its affiliates. All rights reserved.
- * 
+ *
  * Use is subject to license terms.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0. You can also
  * obtain a copy of the License at http://odftoolkit.org/docs/license.txt
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -82,12 +82,17 @@ class ZipHelper {
 						try {
 							zipEntry = inputStream.getNextEntry();
 						} catch (java.util.zip.ZipException e) {
-							// JDK 6 -- the try/catch is workaround for a
+                            // JDK 6 -- the try/catch is workaround for a
 							// specific JDK 5 only problem
-							if (!e.getMessage().contains("missing entry name") && !System.getProperty("Java.version").equals("1.5.0")) {
+                            String eMsg = e.getMessage();
+                            String jVersion = System.getProperty("java.version");
+							if (eMsg != null && jVersion != null && !eMsg.contains("missing entry name")  && !jVersion.equals("1.5.0")) {
 								Logger.getLogger(ZipHelper.class.getName()).info("ZIP ENTRY not found");
-								throw e;
-							}
+								throw new java.util.zip.ZipException("ZIP entry '" + zipEntry.toString() + "': " + eMsg);
+							}else{
+                                Logger.getLogger(ZipHelper.class.getName()).info(e.getMessage());
+                                throw new java.util.zip.ZipException("ZIP entry '" + zipEntry.toString() + "': " + eMsg);
+                            }
 						}
 					}
 				}
