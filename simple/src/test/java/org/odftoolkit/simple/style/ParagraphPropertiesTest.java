27af1256a351193bff0da9f0bbe396f67511e4ee
Svante Schubert <svanteschubert@apache.org>
2016-02-17 17:54:32 +0000

    #ODFTOOLKIT-417# Patch by Georg Fuechsle - Adding setter and getter for paragraph's background-color, subscripted and superscripted
    git-svn-id: https://svn.apache.org/repos/asf/incubator/odf/trunk@1730892 13f79535-47bb-0310-9956-ffa450edef68

--- simple/src/test/java/org/odftoolkit/simple/style/ParagraphPropertiesTest.java
+++ simple/src/test/java/org/odftoolkit/simple/style/ParagraphPropertiesTest.java
@@ -1,4 +1,4 @@
-/* 
+/*
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
@@ -15,49 +15,69 @@ software distributed under the License is distributed on an
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
-*/
-
+ */
 package org.odftoolkit.simple.style;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.junit.Assert;
 import org.junit.Test;
+import org.odftoolkit.odfdom.type.Color;
 import org.odftoolkit.simple.TextDocument;
 import org.odftoolkit.simple.style.StyleTypeDefinitions.HorizontalAlignmentType;
 import org.odftoolkit.simple.text.Paragraph;
+import org.odftoolkit.simple.text.ParagraphStyleHandler;
 import org.odftoolkit.simple.utils.ResourceUtilities;
 
 public class ParagraphPropertiesTest {
-	
-	private static final Logger LOGGER =  Logger.getLogger(ParagraphPropertiesTest.class.getName());
-	static final String filename = "testGetCellAt.ods";
 
-	@Test
-	public void testGetSetBorder() {
-		try {
-			TextDocument doc = TextDocument.newTextDocument();
-			Paragraph paragraph1 = doc.addParagraph("paragraph1");
-			
-			paragraph1.setHorizontalAlignment(HorizontalAlignmentType.JUSTIFY);
-			HorizontalAlignmentType align = paragraph1.getHorizontalAlignment();
-			Assert.assertEquals(HorizontalAlignmentType.JUSTIFY, align);
+    private static final Logger LOGGER = Logger.getLogger(ParagraphPropertiesTest.class.getName());
+    static final String filename = "testGetCellAt.ods";
 
-			paragraph1.setHorizontalAlignment(HorizontalAlignmentType.LEFT);
-			align = paragraph1.getHorizontalAlignment();
-			Assert.assertEquals(HorizontalAlignmentType.LEFT, align);
+    @Test
+    public void testGetSetBorder() {
+        try {
+            TextDocument doc = TextDocument.newTextDocument();
+            Paragraph paragraph1 = doc.addParagraph("paragraph1");
 
-			paragraph1.setHorizontalAlignment(HorizontalAlignmentType.RIGHT);
-			align = paragraph1.getHorizontalAlignment();
-			Assert.assertEquals(HorizontalAlignmentType.RIGHT, align);
+            paragraph1.setHorizontalAlignment(HorizontalAlignmentType.JUSTIFY);
+            HorizontalAlignmentType align = paragraph1.getHorizontalAlignment();
+            Assert.assertEquals(HorizontalAlignmentType.JUSTIFY, align);
 
-			doc.save(ResourceUtilities.newTestOutputFile("TestParagraphPropertiesSetGetHoriAlignment.odt"));
-		} catch (Exception e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			Assert.fail();
-		}
+            paragraph1.setHorizontalAlignment(HorizontalAlignmentType.LEFT);
+            align = paragraph1.getHorizontalAlignment();
+            Assert.assertEquals(HorizontalAlignmentType.LEFT, align);
 
-	}
-	
+            paragraph1.setHorizontalAlignment(HorizontalAlignmentType.RIGHT);
+            align = paragraph1.getHorizontalAlignment();
+            Assert.assertEquals(HorizontalAlignmentType.RIGHT, align);
+
+            doc.save(ResourceUtilities.newTestOutputFile("TestParagraphPropertiesSetGetHoriAlignment.odt"));
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            Assert.fail();
+        }
+
+    }
+
+    @Test
+    public void testGetSetBackgroundColor() {
+        try {
+            TextDocument doc = TextDocument.newTextDocument();
+            Paragraph paragraph1 = doc.addParagraph("paragraph1");
+
+            ParagraphStyleHandler psh = paragraph1.getStyleHandler();
+            ParagraphProperties paraProp = psh.getParagraphPropertiesForWrite();
+            paraProp.setBackgroundColor(new Color("#FF0000"));
+
+            psh = paragraph1.getStyleHandler();
+            paraProp = psh.getParagraphPropertiesForWrite();
+
+            Assert.assertEquals("#FF0000", paraProp.getBackgroundColorAttribute());
+
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            Assert.fail();
+        }
+    }
 }
