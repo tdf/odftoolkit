071576f98d840c073d8920875f08aaacf967db21
Svante Schubert <svanteschubert@apache.org>
2019-06-03 16:51:27 +0200

    Update JavaDoc generation scripts

--- src/site/copy-javadoc.sh
+++ src/site/copy-javadoc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Copy JavaDoc API from projects - had to been built with 'mvn install' previously..."
-rm -rf ../docs/api/schema2template
-mv ../../generator/schema2template/target/ ../docs/api/schema2template
+echo "Copy generated JavaDoc API from projects into /docs - must been built earlier with 'mvn install'..."
+rm -rf ../../docs/api/schema2template
+mv ../../generator/schema2template/target/apidocs ../../docs/api/schema2template
 
-rm -rf ../docs/api/odfdom
-mv ../../odfdom/target/ ../docs/api/odfdom
+rm -rf ../../docs/api/odfdom
+mv ../../odfdom/target/apidocs ../../docs/api/odfdom
 
-rm -rf ../docs/api/simple
-mv ../../simple/target/ ../docs/api/simple
+rm -rf ../../docs/api/simple
+mv ../../simple/target/apidocs ../../docs/api/simple
 
-rm -rf ../docs/api/taglets
-mv ../../taglets/target/ ../docs/api/taglets
+rm -rf ../../docs/api/taglets
+mv ../../taglets/target/apidocs ../../docs/api/taglets
