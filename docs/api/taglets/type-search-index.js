e8f4ff9dc18c128424b10581c5d47197f7e95167
Svante Schubert <svanteschubert@apache.org>
2019-10-06 22:30:09 +0200

    Generated markdown to HTML

---
+++ docs/api/taglets/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.odftoolkit.odfdom.taglet","l":"OdfAttributeTaglet"},{"p":"org.odftoolkit.odfdom.taglet","l":"OdfDatatypeTaglet"},{"p":"org.odftoolkit.odfdom.taglet","l":"OdfElementTaglet"}]
