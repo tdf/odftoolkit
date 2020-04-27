87497a93f83b086cf054598f19258533431ba86c
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2020-04-27 07:50:31 +0200

    Validator: Support ODF 1.3

--- validator/src/main/java/org/odftoolkit/odfvalidator/OdfValidatorMode.java
+++ validator/src/main/java/org/odftoolkit/odfvalidator/OdfValidatorMode.java
@@ -39,7 +39,7 @@ public enum OdfValidatorMode {
     VALIDATE_STRICT ("strict valid"),
 
     /***
-     * ODF 1.2 only, equals CONFORMANCE for ODF 1.0/1.1
+     * ODF 1.2 and 1.3 only, equals CONFORMANCE for ODF 1.0/1.1
      */
     EXTENDED_CONFORMANCE ("extended conformant");
 
