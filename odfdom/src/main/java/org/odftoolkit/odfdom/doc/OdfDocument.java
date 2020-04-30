69de3770cf0a7e0c5942e9af475d4b5b5c7ccc27
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2020-04-30 09:16:17 +0200

    ODF 1.3 new mimetype: Text master template

--- odfdom/src/main/java/org/odftoolkit/odfdom/doc/OdfDocument.java
+++ odfdom/src/main/java/org/odftoolkit/odfdom/doc/OdfDocument.java
@@ -122,6 +122,7 @@ public abstract class OdfDocument extends OdfSchemaDocument {
 		TEXT("application/vnd.oasis.opendocument.text", "odt"),
 		TEXT_MASTER("application/vnd.oasis.opendocument.text-master", "odm"),
 		TEXT_TEMPLATE("application/vnd.oasis.opendocument.text-template", "ott"),
+		TEXT_MASTER_TEMPLATE("application/vnd.oasis.opendocument.text-master-template", "otm"),
 		TEXT_WEB("application/vnd.oasis.opendocument.text-web", "oth");
 		private final String mMediaType;
 		private final String mSuffix;
@@ -404,6 +405,7 @@ public abstract class OdfDocument extends OdfSchemaDocument {
 			case TEXT:
 			case TEXT_TEMPLATE:
 			case TEXT_MASTER:
+			case TEXT_MASTER_TEMPLATE:
 			case TEXT_WEB:
 				documentTemplate = OdfTextDocument.EMPTY_TEXT_DOCUMENT_RESOURCE;
 				break;
@@ -462,6 +464,10 @@ public abstract class OdfDocument extends OdfSchemaDocument {
 			case TEXT_MASTER:
 				newDoc = new OdfTextDocument(pkg, internalPath, OdfTextDocument.OdfMediaType.TEXT_MASTER, enableCollaboration);
 				break;
+
+			case TEXT_MASTER_TEMPLATE:
+			    newDoc = new OdfTextDocument(pkg, internalPath, OdfTextDocument.OdfMediaType.TEXT_MASTER_TEMPLATE, enableCollaboration);
+			    break;
 
 			case TEXT_WEB:
 				newDoc = new OdfTextDocument(pkg, internalPath, OdfTextDocument.OdfMediaType.TEXT_WEB, enableCollaboration);
