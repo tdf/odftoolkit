5cc67b113941b1d97925ce9ee7c4f376847d894a
Svante Schubert <svante.schubert@gmail.com>
2020-05-10 22:55:05 +0200

    RNG HTML update

---
+++ docs/odf1.3/highlight-lines-by-fragment-identifier.js
@@ -0,0 +1,17 @@
+/** The URL might end with a frament identifier (starting with # sign),
+    referenzing to a line number or a span of line numbers! */
+    document.addEventListener("DOMContentLoaded", function() { // wait with JavaScript, unil DOM is available
+        var fi = window.location.hash.substring(1); // fragment identifier behind the '#'
+        if(fi != ''){ // if fragment identifier is not empty
+            if(fi.includes('-')){ // if multiple lines are being selected
+                var lines = fi.split("-");
+                for ( var i = lines[0] ; i <= lines[1] ; i++ ) { // highlight multiple lines
+                    document.querySelector('tr:nth-child(' + i + ')').classList.add("highlight");
+                }
+                document.getElementById(lines[0]).scrollIntoView();
+            }else{ // if fragment identifier is a single line        
+                document.querySelector('tr:nth-child(' + fi + ')').classList.add("highlight");
+                document.getElementById(fi).scrollIntoView();
+            }
+        }
+    })
