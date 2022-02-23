7a139c6fb46fe734fc8d657f27bd8c888aab9710
Svante Schubert <svante.schubert@gmail.com>
2022-02-24 00:59:54 +0100

    Adding bash for single test cobertura coverage creation - yet hard coded and to be moved into own repo

---
+++ odfdom/coverage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+@echo on
+# -e causes the shell to exit if any subcommand or pipeline returns a non-zero status
+# -v causes the shell to view each command
+set -e -v
+
+# -f gives no error if no jacoco.exec exists
+rm -f ./target/jacoco.exec
+rm -rf ./target/site
+
+# ToDo: textFeatureName should vary on input name
+# ToDo: Add variable for MavenRepo Path
+# ToDo: Add variable for JAR version used from pom.xm
+mvn surefire:test -Dtest=FeatureLoadTest -DtextFeatureName=text_italic -DargLine=-javaagent:~/.m2/repository/org/jacoco/org.jacoco.agent/0.8.7/org.jacoco.agent-0.8.7-runtime.jar=destfile=./target/jacoco.exec
+mvn jacoco:report
+
+# ToDo: destination file name should vary on input name
+cp ./target/site/jacoco/jacoco.xml src/test/resources/test-input/feature/coverage/jacoco_text_italic.cov
+
+# ToDo: check in cover2cover.py
+# ToDo: destination file name should vary on input name
+python2 cover2cover.py target/site/jacoco/jacoco.xml src/main/java > src/test/resources/test-input/feature/coverage/cobertura_text_italic.cov
+
