f174bc4b0692012a490b4cf46eee8b9d4440c213
Svante Schubert <svante.schubert@gmail.com>
2022-02-24 01:11:28 +0100

    Adding comments

--- odfdom/coverage.sh
+++ odfdom/coverage.sh
@@ -11,13 +11,19 @@ rm -rf ./target/site
 # ToDo: textFeatureName should vary on input name
 # ToDo: Add variable for MavenRepo Path
 # ToDo: Add variable for JAR version used from pom.xm
+# Triggers a single test 'FeatureLoadTest' with the system property 'textFeatureName' being the trunc name of an ODT test file
+# being loaded from src/test/resources/test-input/feature/text_italic.odt
 mvn surefire:test -Dtest=FeatureLoadTest -DtextFeatureName=text_italic -DargLine=-javaagent:/home/svante/.m2/repository/org/jacoco/org.jacoco.agent/0.8.7/org.jacoco.agent-0.8.7-runtime.jar=destfile=./target/jacoco.exec
+
+# Creates the cobertura XML file and HTML coverage report from the binary target/jacoco.exec file
 mvn jacoco:report
 
 # ToDo: destination file name should vary on input name
+# Save the jacoco XML file using the cov suffix to avoid whitespace lint on XML
 cp ./target/site/jacoco/jacoco.xml src/test/resources/test-input/feature/coverage/jacoco_text_italic.cov
 
 # ToDo: check in cover2cover.py
 # ToDo: destination file name should vary on input name
+# Transform the jacoco XML to cobertura xml file and keeping it using the cov suffix to avoid whitespace lint on XML
 python2 cover2cover.py target/site/jacoco/jacoco.xml src/main/java > src/test/resources/test-input/feature/coverage/cobertura_text_italic.cov
 
