5d323cf58f2bc4e72f9096f9c3304773c88df8ed
Svante Schubert <svante.schubert@gmail.com>
2022-08-30 21:06:24 +0200

    JavaDoc parameter fixes

--- odfdom/src/main/java/org/odftoolkit/odfdom/pkg/OdfElement.java
+++ odfdom/src/main/java/org/odftoolkit/odfdom/pkg/OdfElement.java
@@ -1017,9 +1017,10 @@ public abstract class OdfElement extends ElementNSImpl {
   }
 
   /**
-   * @param true if the text should not count as for component path nor the element root itself.
-   *     This might occur for nested paragraphs or ignored text element (e.g. text:note-citation).
-   *     For instance called by a SAX Component parser, * * * * * * * see <code>
+   * @param isIngoredComponent true if the text should not count as for component path nor the
+   *     element root itself. This might occur for nested paragraphs or ignored text element (e.g.
+   *     text:note-citation). For instance called by a SAX Component parser, * * * * * * * see
+   *     <code>
    *     org.odftoolkit.odfdom.component.OdfFileSaxHandler</code>
    */
   public void ignoredComponent(boolean isIngoredComponent) {
@@ -1258,7 +1259,7 @@ public abstract class OdfElement extends ElementNSImpl {
   /**
    * @param textPosStart the first text level component to be marked, start counting with 0
    * @param textPosEnd the last text level component to be marked, start counting with 0
-   * @param newSelection the element that should embrace the text defined by the positions provided
+   * @param formatChanges the changes to be applied to the text
    */
   public void markText(int textPosStart, int textPosEnd, JSONObject formatChanges) {
     if (formatChanges != null) {
@@ -1351,7 +1352,7 @@ public abstract class OdfElement extends ElementNSImpl {
    * the span of the previous character will be expanded
    *
    * @param newString string to be inserted
-   * @param position text index of the new string
+   * @param textPosStart text index of the new string
    */
   public void insert(String newString, int textPosStart) {
     if (newString != null && !newString.isEmpty()) {
@@ -1369,8 +1370,8 @@ public abstract class OdfElement extends ElementNSImpl {
    * Insert text to a certain position. The text will be appended to the previous position text, so
    * the span of the previous character will be expanded
    *
-   * @param newString string to be inserted
-   * @param position text index of the new string
+   * @param content content to be inserted
+   * @param textPosStart text index of the new content
    */
   private void insertContent(Object content, int textPosStart) { // parameter order?
     if (textPosStart < 0) {
@@ -1577,8 +1578,9 @@ public abstract class OdfElement extends ElementNSImpl {
      * @param posStart the text position where the span starts
      * @param posEnd the text position, where the span ends (one higher as the last component number
      *     to be included)
-     * @param newSpan the span collecting the marked components
-     * @return
+     * @param algorithm is modifying the
+     * @param data optional for the algorithm
+     * @return new adopted currentNode
      */
     private static int checkElementNode(
         Element currentNode,
@@ -1843,7 +1845,7 @@ public abstract class OdfElement extends ElementNSImpl {
        * @param posStart the start position of the span.
        * @param posEnd the end position of the span as string position. NOTE: One higher than the
        *     originally given component position. Therefore always one higher than posStart.
-       * @param newNode
+       * @param formatChanges the changes to be applied to the text
        * @return the position of the next component to be checked
        */
       int mark(
