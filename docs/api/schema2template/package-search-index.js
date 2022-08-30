b66763b9e92a0600a9a2f0e5b7391572360cdaec
Svante Schubert <svante.schubert@gmail.com>
2022-08-30 17:55:33 +0200

    Update of JavaDoc for generator/schema2template project

--- docs/api/schema2template/package-search-index.js
+++ docs/api/schema2template/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"schema2template"},{"l":"schema2template.example.odf"},{"l":"schema2template.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"schema2template"},{"l":"schema2template.grammar"},{"l":"schema2template.grammar.odf"},{"l":"schema2template.template"}];updateSearchResults();
