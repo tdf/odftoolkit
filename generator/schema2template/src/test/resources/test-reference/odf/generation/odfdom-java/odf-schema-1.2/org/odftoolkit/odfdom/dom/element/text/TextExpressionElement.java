ba330c733f11065fb5b30bbdf83ebda5c96dbcca
Svante Schubert <svante.schubert@gmail.com>
2022-09-14 15:13:11 +0200

    Adding removeContent() method to all elements that contain attributes, that represent the content of an element (e.g. by a formula or cell value)

--- generator/schema2template/src/test/resources/test-reference/odf/generation/odfdom-java/odf-schema-1.2/org/odftoolkit/odfdom/dom/element/text/TextExpressionElement.java
+++ generator/schema2template/src/test/resources/test-reference/odf/generation/odfdom-java/odf-schema-1.2/org/odftoolkit/odfdom/dom/element/text/TextExpressionElement.java
@@ -337,4 +337,17 @@ public class TextExpressionElement extends OdfElement {
 			this.appendChild(this.getOwnerDocument().createTextNode(content));
 		}
 	 }
+
+  /** Removes all the content from the element */
+  @Override
+  public void removeContent() {
+    super.removeContent();
+    this.removeAttributeNS(OdfDocumentNamespace.OFFICE.getUri(), "value");
+    this.removeAttributeNS(OdfDocumentNamespace.OFFICE.getUri(), "value-type");
+    this.removeAttributeNS(OdfDocumentNamespace.OFFICE.getUri(), "time-value");
+    this.removeAttributeNS(OdfDocumentNamespace.OFFICE.getUri(), "date-value");
+    this.removeAttributeNS(OdfDocumentNamespace.OFFICE.getUri(), "boolean-value");
+    this.removeAttributeNS("urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0", "value-type");
+    this.removeAttributeNS(OdfDocumentNamespace.TABLE.getUri(), "formula");
+  }
 }
