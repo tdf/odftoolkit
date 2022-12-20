43d708dc5ed1db7ded4985405d0dda63f9a20456
Svante Schubert <svante.schubert@gmail.com>
2022-12-20 13:24:12 +0100

    Simplyfying HTML site generation from markdown

--- src/site/build-html.sh
+++ src/site/build-html.sh
@@ -3,24 +3,12 @@
 echo "Creating HTML from MarkDown as described"
 echo "  in ./website-development.html"
 
-echo 1. Backup none-site related content
-mv "../../docs/api" ../..
-
-echo 2. Remove previous HTML
-rm ../../docs/*.html
-rm ../../docs/odfdom/*.html
-rm ../../docs/simple/*.html
-rm ../../docs/xsltrunner/*.html
-
-echo 3. Built the site..
+echo 1. Built the site..
 #  Build the site
 cd java
 ./gradlew clean createRuntime
 ./scripts/website-cli generate ../../../docs/
 cd ..
-
-echo 4. Restore none-site related content
-mv "../../api" ../../docs
 
 echo
 echo Now you may review the generated website in the '"<ODF_TOOLKIT>/docs/" directory'!
