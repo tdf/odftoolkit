1518c1608713920aeafd236297ccd3925c368643
Michael Stahl <michael.stahl@collabora.com>
2026-01-26 11:05:59 +0100

    site: add 0.12.0 java doc dir to copy-odftoolkit.org.sh

--- src/site/copy-odftoolkit.org.sh
+++ src/site/copy-odftoolkit.org.sh
@@ -15,7 +15,10 @@ dir="$(mktemp -d)"
 echo using "${dir}"
 
 #git archive --format=tar origin/master | tar -x -C "${dir}" docs/
-git archive --format=tar odftoolkit-0.12.0-docs | tar -x -C "${dir}" docs/
+git archive --format=tar v0.13.0 | tar -x -C "${dir}" docs/
+git archive --format=tar --prefix=0.12/ odftoolkit-0.12.0-docs | tar -x -C "${dir}" 0.12/docs/api
+mv "${dir}/0.12/docs/api" "${dir}/docs/api-0.12"
+rm -r "${dir}/0.12"
 git archive --format=tar --prefix=0.11/ odftoolkit-0.11.0 | tar -x -C "${dir}" 0.11/docs/api
 mv "${dir}/0.11/docs/api" "${dir}/docs/api-0.11"
 rm -r "${dir}/0.11"
